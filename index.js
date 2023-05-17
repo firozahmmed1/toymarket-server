d280a4f0b63cba6f367a9b2d072942715057ca46
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-18 00:07:42 +0600

    start-project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port= process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to Toy market');
+});
+
+app.listen(port, ()=>{
+    console.log(`Running Port on:${port}`)
+})
